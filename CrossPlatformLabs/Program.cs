15ceac62a37f25e99c281e55bb46851b0d21531c
Mykhailo Kazarian <misha.kazaryan2@gmail.com>
2024-09-25 10:15:44 +0300

    Hot fix

--- CrossPlatformLabs/Program.cs
+++ CrossPlatformLabs/Program.cs
@@ -15,12 +15,20 @@ namespace CrossPlatformLabs
             string writePath = Path.GetFullPath(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, @"..\..\..\Resources\Write.txt"));
 
             int N, K;
-                
+            string[] input = fileReader.ReadFile(readPath);
             (N,K)= firstLab.ParseNKValues(fileReader.ReadFile(readPath));
+
+            Console.WriteLine($"Input: {readPath}");
+            Console.WriteLine(string.Join(Environment.NewLine, input));
+            Console.WriteLine();
 
             int res = firstLab.CalculateRookPlacements(N, K);
 
-            Console.WriteLine($"N={N},K={K}, res = {res}");
+            Console.WriteLine($"N={N},K={K}, res = {res}\n");
+
+            Console.WriteLine($"Write: {writePath}");
+            Console.WriteLine(res);
+            Console.WriteLine($"\n");
 
             fileWriter.WriteResult(writePath, res.ToString());
         }
