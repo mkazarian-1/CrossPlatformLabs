6342ca170f29c3fdf17538cca252ae1f72c4708d
Mykhailo Kazarian <misha.kazaryan2@gmail.com>
2024-11-10 21:43:17 +0200

    Labs update

--- Lab1/Program.cs
+++ Lab1/Program.cs
@@ -8,32 +8,11 @@ namespace Lab1
         static void Main(string[] args)
         {
             FirstLab firstLab = new FirstLab();
-            FileReader fileReader = new FileReader();
-            FileWriter fileWriter = new FileWriter();
 
-            string readPath = Path.GetFullPath(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, @"..\..\..\Resources\Read.txt"));
-            string writePath = Path.GetFullPath(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, @"..\..\..\Resources\Write.txt"));
-
-            int N, K;
-            string[] input = fileReader.ReadFile(readPath);
-
-            Console.WriteLine($"Input: {readPath}");
-            Console.WriteLine(string.Join(Environment.NewLine, input));
-            Console.WriteLine();
-
-            if (!firstLab.ValidateInputForRooks(input)) return;
-
-            (N, K) = firstLab.ParseNKValues(fileReader.ReadFile(readPath));
-
-            int res = firstLab.CalculateRookPlacements(N, K);
-
-            Console.WriteLine($"N={N},K={K}, res = {res}\n");
-
-            Console.WriteLine($"Write: {writePath}");
-            Console.WriteLine(res);
-            Console.WriteLine($"\n");
-
-            fileWriter.WriteResult(writePath, res.ToString());
+            string inputPath = Path.GetFullPath(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, @"..\..\..\Resources\Read.txt"));
+            string outputPath = Path.GetFullPath(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, @"..\..\..\Resources\Write.txt"));
+            
+            firstLab.RunLab(inputPath, outputPath);
         }
     }
 }
--- Lab2/Program.cs
+++ Lab2/Program.cs
@@ -8,34 +8,11 @@ namespace Lab2
         static void Main(string[] args)
         {
             SecondLab secondLab = new SecondLab();
-            FileReader fileReader = new FileReader();
-            FileWriter fileWriter = new FileWriter();
 
-            string readPath = Path.GetFullPath(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, @"..\..\..\Resources\Read.txt"));
-            string writePath = Path.GetFullPath(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, @"..\..\..\Resources\Write.txt"));
+            string inputPath = Path.GetFullPath(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, @"..\..\..\Resources\Read.txt"));
+            string outputPath = Path.GetFullPath(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, @"..\..\..\Resources\Write.txt"));
 
-            int N;
-            int[,] grid;
-            char[,] result;
-
-            string[] input = fileReader.ReadFile(readPath);
-
-            Console.WriteLine($"Input: {readPath}");
-            Console.WriteLine(string.Join(Environment.NewLine, input));
-            Console.WriteLine();
-
-            if (!secondLab.ValidateInputForMinPath(input)) return;
-
-            (N, grid) = secondLab.Parser(fileReader.ReadFile(readPath));
-
-            result = secondLab.Solve(N,grid);
-            string res = secondLab.ResultFormation(result);
-
-            Console.WriteLine($"Write: {writePath}");
-            Console.WriteLine(res);
-            Console.WriteLine($"\n");
-
-            fileWriter.WriteResult(writePath, res.ToString());
+            secondLab.RunLab(inputPath, outputPath);
         }
     }
 }
